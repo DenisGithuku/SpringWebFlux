f2d5790c59f8690e0eca5cb8d16dc14c6e28c7f3
githukudenis <githukudenis@gmail.com>
2025-04-24 11:47:17 +0300

    Adds reactive employee controller

---
+++ src/main/java/com/springcamp/reactive_webflux/reactive_webflux/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.springcamp.reactive_webflux.reactive_webflux.controller;
+
+import com.springcamp.reactive_webflux.reactive_webflux.dto.EmployeeDto;
+import com.springcamp.reactive_webflux.reactive_webflux.entity.Employee;
+import com.springcamp.reactive_webflux.reactive_webflux.mapper.EmployeeMapper;
+import com.springcamp.reactive_webflux.reactive_webflux.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    @PostMapping
+    public Mono<EmployeeDto> saveEmployee(
+            @RequestBody EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Mono<Employee> savedEmployee = employeeRepository.save(employee);
+        return savedEmployee.map(EmployeeMapper::mapToEmployeeDto);
+    }
+}
--- src/main/java/com/springcamp/reactive_webflux/reactive_webflux/mapper/EmployeeMapper.java
+++ src/main/java/com/springcamp/reactive_webflux/reactive_webflux/mapper/EmployeeMapper.java
@@ -2,24 +2,23 @@ package com.springcamp.reactive_webflux.reactive_webflux.mapper;
 
 import com.springcamp.reactive_webflux.reactive_webflux.dto.EmployeeDto;
 import com.springcamp.reactive_webflux.reactive_webflux.entity.Employee;
-import org.springframework.stereotype.Component;
 
 public class EmployeeMapper {
 
-    public EmployeeDto mapToEmployeeDto(Employee employee) {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
         return new EmployeeDto(
                 employee.getId(),
-                employee.getFirstname(),
-                employee.getLastname(),
+                employee.getFirstName(),
+                employee.getLastName(),
                 employee.getEmail()
         );
     }
 
-    public Employee mapToEmployee(EmployeeDto employeeDto) {
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
         return new Employee(
                 employeeDto.getId(),
-                employeeDto.getFirstname(),
-                employeeDto.getLastname(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
                 employeeDto.getEmail()
         );
     }
