8a978ea0e2496076f176b0eb0daf377bd36c9290
githukudenis <githukudenis@gmail.com>
2025-04-24 09:08:16 +0300

    Initial commit

---
+++ src/test/java/com/springcamp/reactive_webflux/reactive_webflux/ReactiveWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcamp.reactive_webflux.reactive_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
